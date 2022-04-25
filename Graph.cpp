f29a40c4f57d4d2cdd7d4b0d6fbc68796204fb74
mattloulou <mattloulou123123@gmail.com>
2022-04-25 18:16:46 -0400

    completed all Graph functions except for k-connected

--- Graph.cpp
+++ Graph.cpp
@@ -1,11 +1,10 @@
 #include "Graph.h"
 #include <algorithm>
+#include <iomanip>
 
+Graph::Graph(DynamicArray<DynamicArray<Vertex>> other_adj_list) : adj_list_{ other_adj_list } {}
 
-Graph::Graph(DynamicArray<DynamicArray<int>> other_adj_list);
-
-
-int Graph::VertexDeg(int vertex) const
+int Graph::VertexDeg(Vertex vertex) const
 {
     assert(0 <= vertex && vertex < adj_list_.Size());
     return adj_list_[vertex].Size();
@@ -29,67 +28,73 @@ int Graph::EdgeCount() const
     return total / 2;
 }
 
-int* Graph::FindEdge(int vertex1, int vertex2)
+//directional only. Gives the reference to vertex2 in vertex1's list
+Vertex* Graph::FindEdge(Vertex vertex1, Vertex vertex2)
 {
-    //check if vertices are within range
+    //check if vertices valid
+    assert(vertex1 != vertex2);
     assert(0 <= vertex1 && vertex1 < adj_list_.Size());
     assert(0 <= vertex2 && vertex2 < adj_list_.Size());
 
-    bool 
-    for(int dest : adj_list_[vertex1])
+    for(Vertex& dest : adj_list_[vertex1])
+    {
+        if (dest == vertex2) return &dest;
+    }
+
+    return nullptr;
+}
+
+bool Graph::HasEdge(Vertex vertex1, Vertex vertex2) const
+{
+    //check if vertices valid
+    assert(vertex1 != vertex2);
+    assert(0 <= vertex1 && vertex1 < adj_list_.Size());
+    assert(0 <= vertex2 && vertex2 < adj_list_.Size());
+
+    for (const Vertex& dest : adj_list_[vertex1])
     {
         if (dest == vertex2) return true;
     }
-
     return false;
 }
 
 //returns success if legal edge
-bool Graph::AddEdge(int vertex1, int vertex2)
+bool Graph::AddEdge(Vertex vertex1, Vertex vertex2)
 {
-    //check in range
+    //check if vertices valid
+    assert(vertex1 != vertex2);
     assert(0 <= vertex1 && vertex1 < adj_list_.Size());
     assert(0 <= vertex2 && vertex2 < adj_list_.Size());
 
+
     //check if already exists
-    if(HasEdge(vertex1, vertex2)) return false;
+    if(!FindEdge(vertex1, vertex2)) return false;
     
     //otherwise add new edge
     adj_list_[vertex1].PushBack(vertex2);
     return true;
 }   
 
-bool Graph::RemoveEdge(int vertex1, int vertex2)
+bool Graph::RemoveEdge(Vertex vertex1, Vertex vertex2)
 {
-    //check in range
+    //check if vertices valid
+    assert(vertex1 != vertex2);
     assert(0 <= vertex1 && vertex1 < adj_list_.Size());
     assert(0 <= vertex2 && vertex2 < adj_list_.Size());
 
-    //check if already exists
-    if(!HasEdge(vertex1, vertex2)) return false;
-    
-    bool found = false;
-    for (int& vertex : adj_list_[vertex1]) {
-        if (vertex == vertex2) {
-            vertex = adj_list_[vertex1].Back();
-            adj_list_.PopBack();
-            found = true;
-            break;
-        }
-    }
 
-    if(!found) return false;
-    found = false;
+    //get the edge pointers. FindEdge checks the bounds automatically
+    Vertex* v2 = FindEdge(vertex1, vertex2);
+    Vertex* v1 = FindEdge(vertex2, vertex1);
+    if (!v1 || !v2) return false;
 
-    for (int& vertex : adj_list_[vertex2]) {
-        if (vertex == vertex1) {
-            vertex = adj_list_[vertex2].Back();
-            adj_list_.PopBack();
-            break;
-        }
-    }
-    
-    return 
+    *v1 = adj_list_[vertex2].Back();
+    *v2 = adj_list_[vertex1].Back();
+
+    adj_list_[vertex2].PopBack();
+    adj_list_[vertex1].PopBack();
+
+    return true;
 }
 
 void Graph::Clear()
@@ -97,16 +102,37 @@ void Graph::Clear()
     adj_list_.Clear();
 }
 
-bool Graph::IsKVertexConnected(int k) const; 
+bool Graph::IsKVertexConnected(int k) const
+{
+    return 1;
+}
 
+void Graph::PrintGraph() const
+{
+    for (Vertex vertex1 = 0; vertex1 < VertexCount(); ++vertex1) {
+        std::cout << vertex1 << ": ";
+        for (const Vertex& vertex2 : adj_list_[vertex1]) {
+            std::cout << std::setw(5) << vertex2;
+        }
+        std::cout << '\n';
+    }
+
+    std::cout<<std::flush;
+}
 
 bool Graph::IsWellformed() const //representation invariant
 {
     //make sure graph is bidirectional, and that all veritices are from [0,VertexCount)
-    const int vertexCount = VertexCount();
-    for (int i = 0; i < vertexCount - 1; ++i) {
-        for (int j = 0; j < adj_list_[i].Size(); ++j) {
-            assert(HasEdge(j, i));
+    for (Vertex i = 0; i < VertexCount(); ++i) {
+        for (const Vertex& j : adj_list_[i]) {
+
+            //no self-edges
+            assert(i != j); 
+
+            //check for i in j's adjacency list.
+            assert(HasEdge(j, i)); 
         }
     }
+
+    return true;
 }
--- Graph.h
+++ Graph.h
@@ -1,24 +1,28 @@
 #pragma once
 #include "DynamicArray.h"
 
+using Vertex = int;
+
 class Graph
 {
-	DynamicArray<DynamicArray<int>> adj_list_;
+	DynamicArray<DynamicArray<Vertex>> adj_list_;
 
 public:
 
 
-	Graph(DynamicArray<DynamicArray<int>> other_adj_list);
+	Graph(DynamicArray<DynamicArray<Vertex>> other_adj_list);
 
 
-	int VertexDeg(int vertex) const;
+	int VertexDeg(Vertex vertex) const;
 	int VertexCount() const;
 	int EdgeCount() const;
 
-	int* FindEdge(int vertex1, int vertex2);
-	bool AddEdge(int vertex1, int vertex2); //returns success if legal edge
-	bool RemoveEdge(int vertex1, int vertex2);
+	Vertex* FindEdge(Vertex vertex1, Vertex vertex2);
+	bool HasEdge(Vertex vertex1, Vertex vertex2) const;
+	bool AddEdge(Vertex vertex1, Vertex vertex2); //returns success if legal edge
+	bool RemoveEdge(Vertex vertex1, Vertex vertex2);
 
+	void PrintGraph() const;
 	void Clear();
 
 	bool IsKVertexConnected(int k) const; 
